1006ad21ee1af03d9c3ce513919700ede748c1e8
Mihainuan de Sá Silva <mihainuansilva@gmail.com>
2019-03-10 17:30:54 -0300

    Add-Migration AddedAllConfigurations -verbose Update-Database -Verbose

--- Cuscuzeria.Infra.Data/Migrations/20190310203013_AddedAllConfigurations.Designer.cs
+++ Cuscuzeria.Infra.Data/Migrations/20190310203013_AddedAllConfigurations.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Cuscuzeria.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cuscuzeria.Infra.Data.Migrations
 {
     [DbContext(typeof(CuscuzeriaContext))]
-    partial class CuscuzeriaContextModelSnapshot : ModelSnapshot
+    [Migration("20190310203013_AddedAllConfigurations")]
+    partial class AddedAllConfigurations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Cuscuzeria.Infra.Data/Migrations/20190310203013_AddedAllConfigurations.cs
@@ -0,0 +1,244 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cuscuzeria.Infra.Data.Migrations
+{
+    public partial class AddedAllConfigurations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PromoTitle",
+                table: "Promos",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "Cuscuzeiros",
+                newName: "YearsOfExperience");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TypeName",
+                table: "Types",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Types",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Promos",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Promos",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DrinkName",
+                table: "Drinks",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Drinks",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UrlImg",
+                table: "Cuscuzeiros",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CuscuzeiroName",
+                table: "Cuscuzeiros",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Cuscuz",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CuscuzName",
+                table: "Cuscuz",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Beverages",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeverageName",
+                table: "Beverages",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Accompaniments",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccompanimentName",
+                table: "Accompaniments",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Promos",
+                newName: "PromoTitle");
+
+            migrationBuilder.RenameColumn(
+                name: "YearsOfExperience",
+                table: "Cuscuzeiros",
+                newName: "Age");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TypeName",
+                table: "Types",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Types",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Promos",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PromoTitle",
+                table: "Promos",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DrinkName",
+                table: "Drinks",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Drinks",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UrlImg",
+                table: "Cuscuzeiros",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CuscuzeiroName",
+                table: "Cuscuzeiros",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Cuscuz",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CuscuzName",
+                table: "Cuscuz",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Beverages",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeverageName",
+                table: "Beverages",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Accompaniments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccompanimentName",
+                table: "Accompaniments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+        }
+    }
+}
