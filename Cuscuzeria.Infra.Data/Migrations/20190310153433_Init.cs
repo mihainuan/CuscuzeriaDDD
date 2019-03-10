a95c390a1520e380633b00e6e7f2d8bc2bc536ff
Mihainuan de Sá Silva <mihainuansilva@gmail.com>
2019-03-10 13:06:03 -0300

    Add-Migration Init [SUCCESS] Update-database -Verbose [SUCCESS]

---
+++ Cuscuzeria.Infra.Data/Migrations/20190310153433_Init.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cuscuzeria.Infra.Data.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accompaniments",
+                columns: table => new
+                {
+                    AccompanimentId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AccompanimentName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accompaniments", x => x.AccompanimentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Beverages",
+                columns: table => new
+                {
+                    BeverageId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    BeverageName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beverages", x => x.BeverageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cuscuz",
+                columns: table => new
+                {
+                    CuscuzId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CuscuzName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    AccompanimentId = table.Column<int>(nullable: false),
+                    TypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuscuz", x => x.CuscuzId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cuscuzeiros",
+                columns: table => new
+                {
+                    CuscuzeiroId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CuscuzeiroName = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    UrlImg = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuscuzeiros", x => x.CuscuzeiroId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drinks",
+                columns: table => new
+                {
+                    DrinkId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DrinkName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drinks", x => x.DrinkId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    PromoId = table.Column<int>(nullable: false),
+                    DrinkId = table.Column<int>(nullable: false),
+                    BeverageId = table.Column<int>(nullable: false),
+                    CuscuzId = table.Column<int>(nullable: false),
+                    CuscuzeiroId = table.Column<int>(nullable: false),
+                    OrderStatus = table.Column<int>(nullable: false),
+                    OrderCreation = table.Column<DateTime>(nullable: false),
+                    Delivered = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Promos",
+                columns: table => new
+                {
+                    PromoId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PromoTitle = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    StartsAt = table.Column<DateTime>(nullable: false),
+                    EndsAt = table.Column<DateTime>(nullable: false),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Promos", x => x.PromoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    TypeId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TypeName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Types", x => x.TypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    LastOrder = table.Column<DateTime>(nullable: false),
+                    UrlImg = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accompaniments");
+
+            migrationBuilder.DropTable(
+                name: "Beverages");
+
+            migrationBuilder.DropTable(
+                name: "Cuscuz");
+
+            migrationBuilder.DropTable(
+                name: "Cuscuzeiros");
+
+            migrationBuilder.DropTable(
+                name: "Drinks");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Promos");
+
+            migrationBuilder.DropTable(
+                name: "Types");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
