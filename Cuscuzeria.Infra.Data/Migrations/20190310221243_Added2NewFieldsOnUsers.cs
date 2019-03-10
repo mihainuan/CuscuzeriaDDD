be2cc47be6a45b54ca917c4bf7a9f1b711a18b1e
Mihainuan de Sá Silva <mihainuansilva@gmail.com>
2019-03-10 19:17:44 -0300

    Add-Migration Added2NewFieldsOnUsers2 -verbose Remove-Migration - verbose Update-Database -Verbose

---
+++ Cuscuzeria.Infra.Data/Migrations/20190310221243_Added2NewFieldsOnUsers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cuscuzeria.Infra.Data.Migrations
+{
+    public partial class Added2NewFieldsOnUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Prop1",
+                table: "Usuarios",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Prop2",
+                table: "Usuarios",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prop1",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "Prop2",
+                table: "Usuarios");
+        }
+    }
+}
