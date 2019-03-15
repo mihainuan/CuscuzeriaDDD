6010da2e4de2356a038fba85d7458b2a2a222dc3
Mihainuan de Sá Silva <mihainuansilva@gmail.com>
2019-03-15 06:57:19 -0300

    Renamed & MySqlConn

---
+++ CuscuzeriaDDD.MVC/DB/MySqlConn.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Cuscuzeria.MVC.DB
+{
+    public class MySqlConn
+    {
+        public MySqlConnection conn;
+
+        public MySqlConn()
+        {
+            string connStr = "Server=localhost;userid=root;password=mihai;database=CuscuzeriaDB;";
+            conn = new MySqlConnection(connStr);
+            conn.Open();
+        }
+
+        public DataTable GetData(string _sqlCmd)
+        {
+
+        }
+    }
+}
